85a11c48157546e85e287a64d94498d4cdfba59f
KS <j20032280@student.newinti.edu.my>
2022-12-24 15:02:13 +0800

    Added ssr to home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,64 @@
-import { type NextPage } from "next";
+import { GetServerSideProps, type NextPage } from "next";
 import { trpc } from "../utils/trpc";
 import GuideCard from "../components/GuideCard";
+import { Creator, Guide, Operator, Tag } from "@prisma/client";
+import { prisma } from "../server/db/client";
 
-const Home: NextPage = () => {
-  const data = trpc.guide.getRecentGuides.useQuery(3);
+interface HomeProps {
+  recentGuides:
+    | (Guide & {
+        operators: Operator[];
+        uploadedBy: Creator;
+        tags: Tag[];
+      })[]
+    | undefined;
+}
 
-  if (data.isLoading) return <p>Loading...</p>;
+const Home: NextPage<HomeProps> = ({ recentGuides }) => {
+  // const data = trpc.guide.getRecentGuides.useQuery(3);
+
+  // if (data.isLoading) return <p>Loading...</p>;
 
   return (
     <>
       <h2 className="mb-2 text-2xl font-bold">Recently Added</h2>
       <div className="flex min-w-full flex-col gap-2 md:gap-4">
-        {data.status === "success" ? (
+        {/* {data.status === "success" ? (
           data.data.map((guide) => {
             return <GuideCard guide={guide} key={guide.id} />;
           })
         ) : (
           <p>error</p>
-        )}
+        )} */}
+        {recentGuides?.map((guide) => {
+          return <GuideCard guide={guide} key={guide.id} />;
+        })}
       </div>
     </>
   );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const data = await prisma?.guide.findMany({
+    where: {
+      status: "APPROVED",
+    },
+    orderBy: {
+      submittedAt: "desc",
+    },
+    include: {
+      operators: true,
+      uploadedBy: true,
+      tags: true,
+    },
+    take: 3,
+  });
+
+  return {
+    props: {
+      recentGuides: JSON.parse(JSON.stringify(data)),
+    },
+  };
+};
